7ede9b9d7f02a61498bb7af1dbea99225acb3b4d
tgupta2859 <tgupta2859.au@gmail.com>
2026-04-22 00:33:07 +1000

    time analysis

---
+++ SQL Scripts/Profit by Day.sql
@@ -0,0 +1,14 @@
+SELECT
+    d.date,
+    d.day_name,
+    SUM(f.qty) AS total_unit_sales,
+    SUM(f.dollar_sales_usd) AS total_dollar_sales_usd,
+    SUM(f.margin_usd) AS total_margin_usd
+FROM fact_sales f
+JOIN dim_date d
+    ON f.date_key = d.date_key
+GROUP BY
+    d.date,
+    d.day_name
+ORDER BY
+    total_margin_usd DESC;
